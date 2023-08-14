479c7da46add03e1fb0f2350d7f17f82790af092
7498798088 <128983069+7498798088@users.noreply.github.com>
2023-08-15 00:00:31 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -34,7 +34,7 @@ module "eks" {
     cluster_name    = "jenkins-server-eks-cluster"
     cluster_version = "1.24"
     cluster_endpoint_public_access  = true
-    vpc_id = module.jenkins-server-vpc
+    vpc_id = module.jenkins-server-vpc.vpc_id
     subnet_ids = module.jenkins-server-vpc.private_subnets
 
     tags = {
