f13cdb5cdacc04de928544727249b426d4649e6b
7498798088 <128983069+7498798088@users.noreply.github.com>
2023-08-14 23:38:53 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -6,8 +6,8 @@ module "jenkins-server-vpc" {
   cidr = var.vpc_cidr_block
 
   azs             = data.aws_availabity_zones.azs.names
-  private_subnets = var.private_subnets_cidr_blocks
-  public_subnets  = var.public_subnets_cidr_blocks
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
